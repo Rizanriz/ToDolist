635ec7b6b901c0fb549ae4e42a96f2083956f2b3
Rizanriz <rizanr7777@gmail.com>
2024-04-02 10:50:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function addTask() {
+    var task = document.getElementById("taskInput").value;
+
+    if (!task) {
+        alert("Please enter a task!");
+    }else{
+    document.getElementById("taskList").innerHTML += `<li>${task} 
+    <button onclick="this.parentNode.remove()">Delete</button></li>`
+
+    saveTasksToLocalStorage();
+   }
+}
